b8bd45ee3cf6d271992b280deedd9858a1ef2fc7
Colin Woodbury <colingw@gmail.com>
2015-09-02 11:12:06 -0700

    Added tests for general parser
    - Analysed my own pacman cache for versions

--- test/Test.hs
+++ test/Test.hs
@@ -12,11 +12,12 @@ import Test.Tasty.HUnit
 
 ---
 
-simplest :: IsString s => s
-simplest = "1"
-
-simple :: IsString s => s
-simple = "1.2"
+goodVers :: [Text]
+goodVers = [ "1", "1.2", "1.58.0-3", "20.26.1_0-2", "44.0.2403.157-1"
+           , "0.25-2", "1:0.10.16-3", "8.u51-1", "21-2"
+           , "7.1p1-1", "20150826-1", "1.2.3-alpha.2"
+           , "1.2.3+a1b2c3.1", "1.2.3-alpha.2+a1b2c3.1"
+           ]
 
 badSemVs :: [Text]
 badSemVs = [ "1", "1.2", "1.2.3+a1b2bc3.1-alpha.2", "a.b.c", "1.01.1"
@@ -34,16 +35,13 @@ toCompare = [ "1.0.0-alpha", "1.0.0-alpha.1", "1.0.0-alpha.beta"
 
 suite :: TestTree
 suite = testGroup "Unit Tests"
-  [ testGroup "General Parser"
-    [ testCase simplest $ isometric simplest
-    , testCase simple $ isometric simple
-    , testCase "1.2.3" $ isometric "1.2.3"
-    ]
+  [ testGroup "General Parser" $
+    map (\s -> testCase (unpack s) $ isomorph s) goodVers
   , testGroup "Semantic Versioning"
     [ testGroup "Bad Versions (shouldn't parse)" $
       map (\s -> testCase (unpack s) $ assert $ isLeft $ semver s) badSemVs
     , testGroup "Good Versions (should parse)" $
-      map (\s -> testCase (unpack s) $ isometricSV s) goodSemVs
+      map (\s -> testCase (unpack s) $ isomorphSV s) goodSemVs
     , testGroup "Comparisons" $
       map (\(a,b) -> testCase (unpack $ a <> " < " <> b) $ comp a b) $
       zip toCompare (tail toCompare)
@@ -51,12 +49,12 @@ suite = testGroup "Unit Tests"
   ]
 
 -- | Does pretty-printing return a Version to its original form?
-isometric :: Text -> Assertion
-isometric t = Right t @=? (prettyVer <$> version t)
+isomorph :: Text -> Assertion
+isomorph t = Right t @=? (prettyVer <$> version t)
 
 -- | Does pretty-printing return a SemVer to its original form?
-isometricSV :: Text -> Assertion
-isometricSV t = Right t @=? (prettySemVer <$> semver t)
+isomorphSV :: Text -> Assertion
+isomorphSV t = Right t @=? (prettySemVer <$> semver t)
 
 comp :: Text -> Text -> Assertion
 comp a b = assert $ either (const False) id $ (<) <$> semver a <*> semver b
